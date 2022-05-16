c8df2decdd46ac43b12c4dbd46f7b9b120776943
Jeff Morrison <lbljeffmo@gmail.com>
2022-05-16 15:50:31 -0700

    [server] Send multiple chunked responses

--- streamr_server/src/server.rs
+++ streamr_server/src/server.rs
@@ -1,23 +1,69 @@
 use std::collections::VecDeque;
 use std::net::SocketAddr;
 
+use hyper;
+
 use crate::streamr::Streamr;
+
+async fn streamr_request_handler(
+    _req: hyper::Request<hyper::Body>,
+) -> Result<hyper::Response<hyper::Body>, std::convert::Infallible> {
+    let (mut body_sender, body) = hyper::Body::channel();
+    let response: hyper::Response<hyper::Body> = hyper::Response::new(body);
+
+    println!("Spawning second task...");
+    tokio::spawn(async move {
+        println!("Sending first chunk...");
+        match body_sender.send_data("First chunk...\n".into()).await {
+            Ok(()) => println!("First chunk sent!"),
+            Err(err) => panic!("First chunk failed to send! {:?}", err)
+        };
+        println!("Waiting 3s before second chunk...");
+        tokio::time::sleep(std::time::Duration::from_millis(3000)).await;
+
+        println!("Sending second chunk...");
+        match body_sender.send_data("...second chunk.\n".into()).await {
+            Ok(()) => println!("Second chunk sent!"),
+            Err(err) => panic!("Second chunk failed to send! {:?}", err),
+        };
+        println!("Second task complete");
+    });
+    println!("Returning response object...");
+    Ok(response)
+}
 
 pub struct Server {
     test_streamrs: Option<VecDeque<Streamr>>,
 }
 impl Server {
-  #[allow(dead_code)]
-  pub fn new(_addr: &SocketAddr) -> Self {
-    // TODO1: Start up an http server
-    // Using example here: https://docs.rs/hyper/0.14.16/hyper/server/conn/index.html
+    #[allow(dead_code)]
+    pub async fn new(addr: &SocketAddr) -> Self {
+        // TODO1: Start up an http server
+        // Using example here: https://docs.rs/hyper/0.14.16/hyper/server/conn/index.html
 
-    // TODO2: Wire the http server into self.stream_handler
+        let streamr_makeservice = hyper::service::make_service_fn(
+            |_conn: &hyper::server::conn::AddrStream| {
+                async {
+                    Ok::<_, std::convert::Infallible>(hyper::service::service_fn(
+                        streamr_request_handler
+                    ))
+                }
+            }
+        );
 
-    Server {
-        test_streamrs: None,
+        let hyper_server = 
+            hyper::Server::bind(&addr)
+                .http2_only(true)
+                .serve(streamr_makeservice);
+
+        if let Err(e) = hyper_server.await {
+            eprintln!("server error: {}", e);
+        }
+
+        Server {
+            test_streamrs: None,
+        }
     }
-  }
 }
 impl futures::stream::Stream for Server {
     type Item = Streamr;
@@ -58,7 +104,7 @@ mod server_tests {
     #[tokio::test]
     async fn server_listens() {
         let addr = std::net::SocketAddr::from(([127,0,0,1], 3000));
-        let mut server = Server::new(&addr);
+        let mut server = Server::new(&addr).await;
 
         server.set_test_streamrs(vec![
             Streamr::new(),
